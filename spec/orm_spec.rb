a620aa1afcd2a2c4a0dbac975c1bc1efecd8ceb3
Rory Trunkhill <rtrunkhill@gmail.com>
2018-02-18 00:24:18 +0000

    updated orm spc for cp3

--- spec/orm_spec.rb
+++ spec/orm_spec.rb
@@ -1,18 +1,38 @@
-require 'bloc_record'
+require 'bloc_record/selection.rb'
 
 RSpec.describe Class do
-    let(:dummy_class) { Class.new { include Selection } }
-    # before(:each) do
-    #     @dummy_class = Class.new
-    #     @dummy_class.extend(Selection)
-    # end
-      
-    # describe "validation" do
-        it "raise argument errors" do
-            expect(dummy_class.find_one("first")).to eq 1
-        end
+#     let(:dummy_class) { Class.new { extend Selection } }
+    class DummyClass
+    end
+    
+    before(:each) do
+        @dummy_class = Class.new
+        @dummy_class.extend(Selection)
+    end
+          
+        # describe "validation" do
+    it "raises argument errors" do
+        expect { @dummy_class.find_one("first") }.to raise_error(ArgumentError)
+    end
+    
+    it "raises argument errors" do
+        expect { @dummy_class.find("first", 1) }.to raise_error(ArgumentError)
+    end
+    
+    it "calls method_missing correctly" do
+        expect{ @dummy_class.find_by_name(:peter_kirk) }.to raise_error(NameError)
+    end
 
-  
+    #   def method_missing(m, *args, &block)
+    #     m = m.split('_')
+    #     m.map! { |w| w.to_s }
+    #     if m[0] = "find" && m[1] = "by"  && Schema.columns.include?(m[2])
+    #         find_by(m[2], args[1])
+    #     else
+    #         puts "There is no #{m} method"
+    #     end
+    # end
+    
 
 #   describe "#to_s" do
 #     it "prints an entry as a string" do
