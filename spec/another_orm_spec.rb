98042ac35f3eb0b588d76295297fb29b7c750ec5
Rory Trunkhill <rtrunkhill@gmail.com>
2018-02-28 18:52:23 +0000

    testing destroy method

--- spec/another_orm_spec.rb
+++ spec/another_orm_spec.rb
@@ -4,7 +4,10 @@ require 'bloc_record'
 
 BlocRecord.connect_to('db/address_bloc.sqlite')
 
-people = { 1 => { "first_name" => "David" }, 2 => { "first_name" => "Jeremy" } }
+# people = { 1 => { "first_name" => "David" }, 2 => { "first_name" => "Jeremy" } }
 
 # puts Entry.update(1, {:name => "David"})
-puts  Entry.update(people.keys, people.values)
+# puts  Entry.update(people.keys, people.values)
+puts Entry.all
+
+# puts Entry.destroy_all("phone_number = ?", '111-111-1111')
